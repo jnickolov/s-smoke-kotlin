4b6faf7d6b1a86f67a18f63791dc017278262d36
Jordan Nickolov <jordan_nickolov@yahoo.com>
2025-03-02 09:41:59 +0200

    code cleanup

--- app/src/main/java/eu/efficientsoft/lpl/ssmoke/mobileapp/MainActivity.kt
+++ app/src/main/java/eu/efficientsoft/lpl/ssmoke/mobileapp/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import eu.efficientsoft.lpl.ssmoke.mobileapp.app.Home
 import eu.efficientsoft.lpl.ssmoke.mobileapp.app.SSmokeApp
 import eu.efficientsoft.lpl.ssmoke.mobileapp.app.SSmokeTopBar
@@ -18,38 +20,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val drawerState = rememberDrawerState(DrawerValue.Closed)
 
+        setContent {
             SSmokeMobileAppTheme {
-                Scaffold (
-                    topBar = { SSmokeTopBar (drawerState) },
-                    ) {
-                    Surface(modifier = Modifier.padding(it)) {
-                        SSmokeApp (pageRoute = Home)
-                    }
-                }
+                SSmokeApp()
             }
 
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//    Button(onClick = { /*TODO*/ }) {
-//        Text("OpenDrawer")
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    SSmokeMobileAppTheme {
-//        Greeting("Android")
-//    }
-//}
--- app/src/main/java/eu/efficientsoft/lpl/ssmoke/mobileapp/app/SSmokeApp.kt
+++ app/src/main/java/eu/efficientsoft/lpl/ssmoke/mobileapp/app/SSmokeApp.kt
@@ -1,6 +1,18 @@
 package eu.efficientsoft.lpl.ssmoke.mobileapp.app
 
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,6 +25,7 @@ import eu.efficientsoft.lpl.ssmoke.mobileapp.features.NewAccountScreen
 import eu.efficientsoft.lpl.ssmoke.mobileapp.features.NotificationsScreen
 import eu.efficientsoft.lpl.ssmoke.mobileapp.features.SettingsScreen
 import eu.efficientsoft.lpl.ssmoke.mobileapp.features.UserProfileScreen
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -36,9 +49,51 @@ object NewAccount
 
 
 @Composable
-fun SSmokeApp(pageRoute: Any) {
+fun SSmokeApp() {
    // Log.i("IN SSMOKE APP", pageRoute.toString())
 
+//    val navController = rememberNavController()
+//
+//    NavHost(navController, startDestination = Home) {
+//        composable<Home> { HomeScreen() }
+//        composable<Devices> { DevicesScreen() }
+//        composable<Notifications> { NotificationsScreen() }
+//        composable<Events> { EventsScreen() }
+//        composable<Settings> { SettingsScreen() }
+//        composable<Help> { HelpScreen() }
+//        composable<Login> { LoginScreen(onNewAccount = { navController.navigate(NewAccount) }) }
+//        composable<Profile> { UserProfileScreen() }
+//        composable<NewAccount> { NewAccountScreen() }
+//    }
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val pageRoute: MutableState<Any> = remember { mutableStateOf (Home) }
+
+
+    Scaffold (
+        topBar = { SSmokeTopBar (
+            onMenuClick = {
+                Log.i("Navigation Menu Button", "Opening drawer")
+                scope.launch { drawerState.open() }
+            },
+            onActionClick = {}
+        ) },
+    ) {
+        Surface(modifier = Modifier.padding(it)) {
+            SSmokeDrawer(
+                drawerState = drawerState,
+                onSelect = { navId ->
+                    scope.launch {drawerState.close()}
+                    pageRoute.value = navId
+                }) {
+                SSmokeNavigationScreen(pageRoute.value)
+            }
+        }
+    }
+}
+
+@Composable fun SSmokeNavigationScreen (route: Any) {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = Home) {
@@ -48,11 +103,10 @@ fun SSmokeApp(pageRoute: Any) {
         composable<Events> { EventsScreen() }
         composable<Settings> { SettingsScreen() }
         composable<Help> { HelpScreen() }
-        composable<Login> { LoginScreen (onNewAccount = { navController.navigate(NewAccount) })}
+        composable<Login> { LoginScreen(onNewAccount = { navController.navigate(NewAccount) }) }
         composable<Profile> { UserProfileScreen() }
         composable<NewAccount> { NewAccountScreen() }
     }
 
-    navController.navigate(pageRoute)
+    navController.navigate(route)
 }
-
